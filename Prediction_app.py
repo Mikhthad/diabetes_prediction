a5e454aa5c1f8f8291fb7862078f65a992e8e285
Mikhthad T T <mikhthadmikku06@gmail.com>
2025-03-03 15:07:26 +0530

    Update Prediction_app.py

--- Prediction_app.py
+++ Prediction_app.py
@@ -15,18 +15,17 @@ st.markdown("""
             color: #333333;
         }
         .main {
-    background-color: white;
-    background-image: url("https://raw.githubusercontent.com/Mikhthad/diabetes_prediction/main/3701981.webp");
-    background-size: cover;
-    background-blend-mode: overlay;
-    background-color: rgba(1, 1, 1, 0.002); /* Adjust transparency */
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
-}
-
+            background-color: white;
+            background-image: url(https://wallpapercave.com/wp/wp2616571.jpg);
+            background-size: cover;
+            background-blend-mode: overlay;
+            background-color: rgba(1, 1, 1, 0.002); /* Adjust transparency */
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+        }
         h1 {
-            color: black !important;
+            color: black;
             text-align: center;
         }
         /* Style button */
