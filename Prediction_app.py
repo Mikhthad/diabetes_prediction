016accc4714b8e5f70d64f74a393c73c4e813cc1
Mikhthad T T <mikhthadmikku06@gmail.com>
2025-03-03 15:14:39 +0530

    Update Prediction_app.py

--- Prediction_app.py
+++ Prediction_app.py
@@ -18,7 +18,7 @@ st.markdown("""
             background-color: white;
             background-image: url("https://github.com/Mikhthad/diabetes_prediction/blob/master/3701981.webp");
             background-size: cover;
-            background-blend-mode: overlay !important;
+            background-blend-mode: overlay;
             background-color: rgba(1, 1, 1, 0.002); /* Adjust transparency */
             padding: 20px;
             border-radius: 10px;
